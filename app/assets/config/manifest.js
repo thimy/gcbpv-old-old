a9b4a120268f84449ba34bc1edfb999bb1d93523
Thimy Kieu <thimy@bump.sh>
2023-07-03 13:12:17 +0200

    fix assets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,7 +2,9 @@
 //= link_tree ../builds
 
 //= link_tree ../../components .js
-//= link application.css
+//= link_directory ../stylesheets .css
 //= link administrate-field-simple_markdown/application.css
 //= link administrate-field-simple_markdown/application.js
 //= link administrate-field-active_storage/application.css
+
+//= link_tree controllers .js
